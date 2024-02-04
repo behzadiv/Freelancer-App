b198f4e65273183c89e075c034d889103ad69ab0
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-04 20:28:16 +0330

    feat : add component layer for textField & button

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const SendOTPForm = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
-  console.log("phon", phoneNumber);
+  
   return (
     <div className="w-full sm:max-w-screen-sm mx-auto space-y-4">
       <div>
@@ -10,14 +10,14 @@ const SendOTPForm = () => {
           شماره موبایل
         </label>
         <input
-          type="number"
+          type="text"
           id="phoneNumber"
           value={phoneNumber}
           onChange={(e) => setPhoneNumber(e.target.value)}
-          className="w-full py-2 px-4 text-primary-800 border border-gray-300 rounded-xl hover:border-primary-300 focus:border-primary-300 focus:shadow-md focus:shadow-primary-200 transition-all duration-300 ease-out"
+          className="textField_input"
         />
       </div>
-      <button className="w-full bg-primary-900 font-bold text-white py-2 rounded-xl hover:bg-primary-800 transition-all duration-300 shadow-lg  shadow-primary-300">
+      <button className="btn btn-primary w-full">
         ارسال کد تایید
       </button>
     </div>
