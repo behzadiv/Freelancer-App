3e24643f1e3977a9d8469f9adbdb68c5af5b98b0
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-04 16:18:37 +0330

    feat : add colors to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className='text-red-400'>Freelance - App</h1>
+    <h1 className='text-primary-900 text-4xl  font-bold bg-red-300 container'>Freelance - App</h1>
   )
 }
 
