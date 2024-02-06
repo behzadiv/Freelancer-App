6df492012542d6e91b386d88beaf67b21582b8a3
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-06 20:08:50 +0330

    pkg : install react-hot-toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Auth from "./pages/Auth";
 import "./App.css";
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 const queryClinet = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClinet}>
+      <Toaster />
       <div className="container">
         <Routes>
           <Route path="/auth" element={<Auth />} />
