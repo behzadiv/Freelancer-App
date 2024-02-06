aeb6b6a0adc10d98be2c412ee18afba737547fe3
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-06 18:59:41 +0330

    feta : install react-query and use it in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Auth from "./pages/Auth";
 import "./App.css";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const queryClinet = new QueryClient();
 
 function App() {
   return (
-    <div className="container">
-      <Routes>
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </div>
+    <QueryClientProvider client={queryClinet}>
+      <div className="container">
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </div>
+    </QueryClientProvider>
   );
 }
 
