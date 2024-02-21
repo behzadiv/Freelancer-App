ee7972142abdc787a415905dd70faf17ebf092a1
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-21 13:42:06 +0330

    feat : create custom navLink

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,36 +2,20 @@ import { NavLink } from "react-router-dom";
 import { HiHome, HiCollection } from "react-icons/hi";
 
 const Sidebar = () => {
-  const navLinkClass =
-    "font-bold flex gap-x-2 py-1 px-5 rounded-md items-center hover:bg-primary-100/50 hover:text-primary-900 transition-all duration-300";
   return (
     <div className="bg-secondary-0 row-start-1 row-span-2 border border-slate-100 pt-8 flex justify-center">
       <ul className="w-11/12 space-y-2">
         <li>
-          <NavLink
-            to="dashboard"
-            className={({ isActive }) => {
-              return isActive
-                ? `${navLinkClass} bg-primary-50 text-primary-800`
-                : `${navLinkClass} text-secondary-600`;
-            }}
-          >
+          <CustomNavLink to={"/owner/dashboard"}>
             <HiHome />
             <span>داشبورد</span>
-          </NavLink>
+          </CustomNavLink>
         </li>
         <li>
-          <NavLink
-            to="projects"
-            className={({ isActive }) => {
-              return isActive
-                ? `${navLinkClass} bg-primary-50 text-primary-800`
-                : `${navLinkClass} text-secondary-600`;
-            }}
-          >
+          <CustomNavLink to={"/owner/projects"}>
             <HiCollection />
             <span>پروژه ها</span>
-          </NavLink>
+          </CustomNavLink>
         </li>
       </ul>
     </div>
@@ -39,3 +23,21 @@ const Sidebar = () => {
 };
 
 export default Sidebar;
+
+function CustomNavLink({ children, to }) {
+  const navLinkClass =
+    "font-bold flex gap-x-2 py-1 px-5 rounded-md items-center hover:bg-primary-100/50 hover:text-primary-900 transition-all duration-300";
+
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) => {
+        return isActive
+          ? `${navLinkClass} bg-primary-50 text-primary-800`
+          : `${navLinkClass} text-secondary-600`;
+      }}
+    >
+      {children}
+    </NavLink>
+  );
+}
