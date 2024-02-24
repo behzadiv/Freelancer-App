dde791abb3271e12e19ad182804a9f31dc3657d1
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-24 18:07:12 +0330

    fix : fix style of modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -6,9 +6,9 @@ const Modal = ({ children, title, open, onClose }) => {
       <>
         <div
           onClick={onClose}
-          className="backdrop-blur-sm bg-secondary-800 w-full h-screen fixed top-0 left-0  bg-opacity-30"
+          className="backdrop-blur-sm bg-secondary-800 w-full h-screen fixed top-0 left-0  bg-opacity-30 z-40"
         ></div>
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-secondary-0 w-64 p-4 rounded-lg shadow-md z-50">
+        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-secondary-0 p-4 rounded-lg shadow-md z-50 w-[calc(100vw-2rem)] md:max-w-lg max-h-[calc(100vh-2rem)] overflow-y-auto">
           <div className="flex items-center justify-between border-b border-b-secondary-300 pb-2 mb-6 font-bold text-base">
             <p>{title}</p>
             <button onClick={onClose}>
