089077f6b7b2215851340060b419d379c19395f4
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-24 15:07:53 +0330

    fix : close modal when clicked outside

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -3,11 +3,12 @@ import { IoClose } from "react-icons/io5";
 const Modal = ({ children, title, open, onClose }) => {
   return (
     open && (
-      <div
-        onClick={onClose}
-        className="backdrop-blur-sm bg-secondary-800 w-full h-screen fixed top-0 left-0  z-50  bg-opacity-30"
-      >
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-secondary-0 w-64 p-4 rounded-lg shadow-md">
+      <>
+        <div
+          onClick={onClose}
+          className="backdrop-blur-sm bg-secondary-800 w-full h-screen fixed top-0 left-0  bg-opacity-30"
+        ></div>
+        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-secondary-0 w-64 p-4 rounded-lg shadow-md z-50">
           <div className="flex items-center justify-between border-b border-b-secondary-300 pb-2 mb-6 font-bold text-base">
             <p>{title}</p>
             <button onClick={onClose}>
@@ -16,7 +17,7 @@ const Modal = ({ children, title, open, onClose }) => {
           </div>
           {children}
         </div>
-      </div>
+      </>
     )
   );
 };
