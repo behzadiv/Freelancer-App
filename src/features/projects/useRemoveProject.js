3dccaef3359042fa23f27f722c08dbd1aacb591c
behzad <90694618+behzadiv@users.noreply.github.com>
2024-02-24 20:18:22 +0330

    feat : invalidate query in order to get data again when project removed

--- src/features/projects/useRemoveProject.js
+++ src/features/projects/useRemoveProject.js
@@ -1,12 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { removeProjectApi } from "../../services/projectsService";
 import toast from "react-hot-toast";
 
 export default function useRemoveProject() {
+  const queryClient = useQueryClient();
+
   const { isPending: isDeleting, mutate: removeProject } = useMutation({
     mutationFn: removeProjectApi,
     onSuccess: (data) => {
       toast.success(data.message);
+      queryClient.invalidateQueries(["get-projects"]);
     },
     onError: (err) => {
       toast.error(err?.response?.data?.message);
