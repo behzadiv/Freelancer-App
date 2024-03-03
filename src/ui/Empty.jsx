0b4ed8d98adab02b96028a6ecbf1fb47cbec0000
behzad <90694618+behzadiv@users.noreply.github.com>
2024-03-03 16:50:36 +0330

    feat : create Empty ui component

---
+++ src/ui/Empty.jsx
@@ -0,0 +1,9 @@
+const Empty = ({ resourceName }) => {
+  return (
+    <div className="pt-5">
+      <p className="text-secondary-700 font-bold">{resourceName} یافت نشد.</p>
+    </div>
+  );
+};
+
+export default Empty;
