2d9943dada94e6e5e98e19f5affc2680c2487e49
behzad <90694618+behzadiv@users.noreply.github.com>
2024-03-03 15:49:46 +0330

    feat : develope ProjectHeader component

--- src/features/project/ProjectHeader.jsx
+++ src/features/project/ProjectHeader.jsx
@@ -1,5 +1,18 @@
+import { HiArrowRight } from "react-icons/hi";
+import useMoveBack from "../../hooks/useMoveBack";
+
 const ProjectHeader = ({ project }) => {
-  return <div></div>;
+  const moveBack = useMoveBack();
+  return (
+    <div className="flex items-center gap-x-4">
+      <button onClick={moveBack}>
+        <HiArrowRight className="w-5 h-5" />
+      </button>
+      <p className="font-black text-secondary-700 text-lg">
+        لیست درخواست های {project.title}
+      </p>
+    </div>
+  );
 };
 
 export default ProjectHeader;
