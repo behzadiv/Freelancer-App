41f8a2770ebadf83e5ac47e9d422ea30ee9eed17
behzad <90694618+behzadiv@users.noreply.github.com>
2024-03-05 15:04:22 +0330

    feat : set darkMode in localStorage

--- src/context/DarkModeProvider.jsx
+++ src/context/DarkModeProvider.jsx
@@ -1,17 +1,28 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const DarkModeContext = createContext();
 
 const DarkModeProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const storedValue = JSON.parse(localStorage.getItem("isDarkMode"));
+  
+  const [isDarkMode, setIsDarkMode] = useState(storedValue || false);
   const toggleDarkMode = () => {
     setIsDarkMode((prev) => !prev);
     if (!isDarkMode) {
       document.documentElement.classList.add("dark-mode");
+      localStorage.setItem("isDarkMode", JSON.stringify(true));
+    } else {
+      document.documentElement.classList.remove("dark-mode");
+      localStorage.setItem("isDarkMode", JSON.stringify(false));
+    }
+  };
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
     } else {
       document.documentElement.classList.remove("dark-mode");
     }
-  };
+  }, [isDarkMode]);
 
   return (
     <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
