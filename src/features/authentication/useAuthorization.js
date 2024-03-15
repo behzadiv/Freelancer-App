0e0e0da2e2c83caabaca37842106a3414a3c2be0
behzad <90694618+behzadiv@users.noreply.github.com>
2024-03-15 13:21:26 +0330

    feat : create isVeryfied option to redirect to not access page when user.status !== 2

--- src/features/authentication/useAuthorization.js
+++ src/features/authentication/useAuthorization.js
@@ -11,6 +11,8 @@ export default function useAuthorization() {
   const { pathname } = useLocation();
   let isAuthenticated = false;
   let isAuthorized = false;
+  let isVeryfied = false;
+
   const desiredRoll = pathname.split("/").at(1);
 
   if (user) {
@@ -19,6 +21,7 @@ export default function useAuthorization() {
   if (Object.keys(ROLLS).includes(desiredRoll)) {
     if (user && user.role === ROLLS[desiredRoll]) isAuthorized = true;
   }
+  if (user && user.status === 2) isVeryfied = true;
 
-  return { isAuthenticated, isAuthorized, isLoading };
+  return { isAuthenticated, isAuthorized, isLoading, isVeryfied };
 }
--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -2,15 +2,21 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "./Loading";
 import useAuthorization from "../features/authentication/useAuthorization";
+import toast from "react-hot-toast";
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, isAuthorized, isLoading } = useAuthorization();
+  const { isAuthenticated, isAuthorized, isLoading, isVeryfied } =
+    useAuthorization();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isAuthenticated && !isLoading) navigate("/auth");
+    if (!isVeryfied && !isLoading) {
+      toast.error("پروفایل شما هنوز تایید نشده است.");
+      navigate("/not-access");
+    }
     if (!isAuthorized && !isLoading) navigate("/not-access", { replace: true });
-  }, [isAuthenticated, isAuthorized, isLoading, navigate]);
+  }, [isAuthenticated, isAuthorized, isLoading, navigate, isVeryfied]);
 
   if (isLoading)
     return (
